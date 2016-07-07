d33d4e5477f03cb6a9647592aa24c66c21730624
George Kyle <gskyle@gmail.com>
2016-07-07 19:13:05 +0100

    Create server.R

---
+++ server.R
@@ -0,0 +1,30 @@
+
+require(shiny)
+
+shinyServer(function(input, output, session) {
+	#global varible on x-axis
+	x <- 0
+	
+	# observes if value of mydata sent from the client changes.  if yes
+	# generate a new random color string and send it back to the client
+	# handler function called 'SendObjectToClientDynamicCallbackHandler'
+
+	
+	autoUpdate <- reactiveTimer(1000,session)
+
+	
+	observe({
+		autoUpdate()
+		x <<- x + 10
+		y = runif(1)
+		
+		#pass data to client as object - x & y are passed to client from server on every second
+		variableToPassClient = sprintf('{"X":"%s", 
+                          "Y": "%s"                          
+                          }', x, y)
+
+		session$sendCustomMessage(type="SendObjectToClientDynamicCallbackHandler",variableToPassClient)
+	})
+	
+ 
+})
